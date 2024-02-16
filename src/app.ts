b615bf551b29a8e1ce3365303446a171061a64be
Anas5006 <anassain13@gmail.com>
2024-02-16 10:24:17 +0530

    add filebased config

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
+import config from 'config'
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.json({ message: config.get('port') });
 });
 
 app.use(globalErrorHandler);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import app from "./app";
 import logger from "./config/logger";
+import config from "config";
 
 const startServer = () => {
-    const PORT = 5502;
+    const PORT: number = config.get("port") || 5502;
+    const host: string = config.get("host") || "localhost";
     try {
-        app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
+        app.listen(PORT, () => {
+            logger.info(`Listening on port ${PORT}`)
+            logger.info(`http://${host}:${PORT}`);
+        });
     } catch (err: unknown) {
         if (err instanceof Error) {
             logger.error(err.message);
